04642151b1615db00290af98f5a0b6885078548b
KelvinQiu802 <kelvinqiu802@outlook.com>
2025-03-21 16:24:23 +0800

    fix: add image domain

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com', 'localhost'],
+    domains: ['images.unsplash.com', 'localhost', 'csi420-01-vm7.ucd.ie'],
   },
   devIndicators: {
     buildActivity: false,
